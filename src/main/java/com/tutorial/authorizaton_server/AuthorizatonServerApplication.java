7a64b80dc90412c9e8071f38554fa65c0d3578cd
Juan Rojas <yo217@hotmail.es>
2024-11-20 08:02:40 -0500

    init :tada: security init

---
+++ src/main/java/com/tutorial/authorizaton_server/AuthorizatonServerApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.authorizaton_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizatonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizatonServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/authorizaton_server/AuthorizatonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.authorizaton_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizatonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
